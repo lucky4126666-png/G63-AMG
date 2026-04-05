47bc7286e0640f4a1b277629c7e65debd31bcb96
Nguyen Long <lucky4126666@gmail.com>
2026-04-05 20:12:38 +0700

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,19 @@
+import sqlite3
 
+conn = sqlite3.connect("data.db", check_same_thread=False)
+
+def init():
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS logs (
+        id INTEGER PRIMARY KEY,
+        user TEXT,
+        text TEXT
+    )
+    """)
+    conn.commit()
+
+def save(user, text):
+    c = conn.cursor()
+    c.execute("INSERT INTO logs (user, text) VALUES (?, ?)", (user, text))
+    conn.commit()
