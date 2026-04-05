6e9ddb2ee366e4cd5f6bb8d3fabe9387a8b8ca25
Nguyen Long <lucky4126666@gmail.com>
2026-04-05 20:35:54 +0700

    Update ai.py

--- backend/ai.py
+++ backend/ai.py
@@ -1,14 +1,18 @@
 from openai import OpenAI
 import os
+from memory import get
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-def ask_ai(text):
-    try:
-        res = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "user", "content": text}]
-        )
-        return res.choices[0].message.content
-    except:
-        return "⚠️ AI lỗi"
+def ask_ai(user_id, text):
+    history = get(user_id)
+
+    messages = [{"role": "user", "content": m} for m in history]
+    messages.append({"role": "user", "content": text})
+
+    res = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages
+    )
+
+    return res.choices[0].message.content
